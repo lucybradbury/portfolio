612c411190b487445000a513b3fb569bb067c5a7
Jack Boyce <jack.boyce@dexda.io>
2018-05-16 13:08:51 +0100

    react-enroute

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Router } from 'react-enroute';
+
+const Home = props => <div> {JSON.stringify(props)} </div>;
+const Project = props => <div> {JSON.stringify(props)} </div>;
+const About = props => <div> {JSON.stringify(props)} </div>;
+const NotFound = props => <div> {JSON.stringify(props)} </div>;
 
 class Root extends React.Component {
   render() {
-    return <div> Hello there </div>;
+    const state = {
+      location: window.location.pathname
+    };
+    return (
+      <Router {...state}>
+        <Route path="/" component={Home} />
+        <Route path="/project/:id" component={Project} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={NotFound} />
+      </Router>
+    );
   }
 }
 
-const node = document.querySelector('[app]');
-render(<Root />, node);
+render(<Root />, document.querySelector('[app]'));
